852a77a1fb283fcb8c264efba6e96e9bd00533e1
afeiship <1290657123@qq.com>
2021-09-25 09:57:53 +0800

    refactor: use template/items

--- public/src/app.tsx
+++ public/src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactAntSelectCascader from '../../src/main';
 import styled from 'styled-components';
-
+import ReactAntSelect from '@jswork/react-ant-select';
 import nx from '@jswork/next';
 import '@jswork/next-key-map';
 
@@ -9,10 +9,20 @@ const Container = styled.div`
   width: 80%;
   margin: 30px auto 0;
 `;
+
+const Item = styled.div`
+  strong {
+    display: inline-block;
+    text-align: left;
+    width: 80px;
+  }
+  margin-bottom: 10px;
+`;
+
 const headers = {
   'Content-Type': 'application/json',
   'authorization':
-    'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1dHlwIjoiTERBUF9VU0VSIiwiYXVkIjpbImFvc3AtZ2F0ZXdheSIsIml5eS1hcGkiLCJhb3QiLCJ2d29wcyIsIm5vdGlmaWNhdGlvbi1jZW50ZXItYWRtaW4iLCJhb2MtYWRtaW4iXSwic3ViIjoiYXJpYy56aGVuZyIsImlzcyI6ImFsbzcuY29tIiwiZXhwIjoxNjMyNTMyMzI3LCJqdGkiOiIyNTIwMmFmYS01Mzc2LTRhM2QtODk1OC1mZTQ1NTMwZjIyMzMifQ.rMDFJ5MUDovDYxiUxN1AGqpiYxsT6uRU-fO7FQBj10NNrI19gY879Fl6yW4T_Uz2LPrTvq50NdztNmvOV2u-FYtDQev0vvtiCYl0U62V2zhoS5KXZtqArbP-fB3FBClNJbKzLy-zFZeJjvVN0SKa8JLn0xWWyFPml7DwWAC-P_c',
+    'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1dHlwIjoiTERBUF9VU0VSIiwiYXVkIjpbImFvc3AtZ2F0ZXdheSIsIml5eS1hcGkiLCJhb3QiLCJ2d29wcyIsIm5vdGlmaWNhdGlvbi1jZW50ZXItYWRtaW4iLCJhb2MtYWRtaW4iXSwic3ViIjoiYXJpYy56aGVuZyIsImlzcyI6ImFsbzcuY29tIiwiZXhwIjoxNjMyNTc1NjA5LCJqdGkiOiI5YmE2NDU3OS0wODRmLTQwOTMtOGI0MC1kMDIwOWYwYmVmZjYifQ.AyromYw2qS5s9vXR7h9Gns9ZQbyg47qMfhuxGJh54h8wcyhDRNiF3IhwpGIAgskYPMVu6m-w5rotG7A4OmHSEfcfRw2vzr0inxDuOxo83eehN38ciI3EgTmIQ-IYKQVUcEVZjVG4uheSnfGcRNgjLApstIRDu57m3aGD7L-1DMs',
   'baseinfo': '0,6847'
 };
 
@@ -49,10 +59,19 @@ const query = [
 ];
 
 export default (props: any) => {
+  const labels = ['课程系列', '课程'];
   return (
     <Container>
       <ReactAntSelectCascader
         query={query}
+        template={({ index, props }) => {
+          return (
+            <Item key={index}>
+              <strong>{labels[index]}</strong>
+              <ReactAntSelect style={{ width: 200 }} {...props} />
+            </Item>
+          );
+        }}
         onChange={(e) => {
           console.log('e:', e.target.value);
         }}
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,42 +1,65 @@
 import noop from '@jswork/noop';
 import classNames from 'classnames';
 import React, { Component } from 'react';
-import filterProps from '@jswork/filter-react-props';
 import ReactList from '@jswork/react-list';
 import ReactAntSelect from '@jswork/react-ant-select';
-import { Simulate } from 'react-dom/test-utils';
-import load = Simulate.load;
 
 const CLASS_NAME = 'react-ant-select-cascader';
 
-// export type ReactAntSelectCascaderProps = {
-//   /**
-//    * The extended className for component.
-//    */
-//   className?: string;
-//   /**
-//    * Default value.
-//    */
-//   value?: object;
-//   /**
-//    * The change handler.
-//    */
-//   onChange?: Function;
-// };
+interface TemplateArgs {
+  index: number;
+  item: any;
+  props: {
+    items: any[];
+    loading: boolean;
+    value: any;
+    onChange: Function;
+  };
+}
 
-export default class ReactAntSelectCascader extends Component<any> {
+export type ReactAntSelectCascaderProps = {
+  /**
+   * The extended className for component.
+   */
+  className?: string;
+  /**
+   * Default value.
+   */
+  value?: any[];
+  /**
+   * The change handler.
+   */
+  onChange?: Function;
+  /**
+   * The api list.
+   * @param args
+   */
+  query: Array<(args: any) => Promise<any>>;
+  /**
+   * The select item template.
+   * @param args
+   */
+  template?: (args: TemplateArgs) => React.ReactNode;
+};
+
+const DEFAULT_TEMPLATE = ({ index, props }) => {
+  return <ReactAntSelect key={index} {...props} />;
+};
+
+export default class ReactAntSelectCascader extends Component<ReactAntSelectCascaderProps, any> {
   static displayName = CLASS_NAME;
   static version = '__VERSION__';
   static defaultProps = {
     query: [],
     value: [],
-    onChange: noop
+    onChange: noop,
+    template: DEFAULT_TEMPLATE
   };
 
   state = {
-    loading: this.props.query.map(() => false),
+    loadingList: this.props.query.map(() => false),
     value: this.props.value,
-    options: this.props.query.map(() => [])
+    itemsList: this.props.query.map(() => [])
   };
 
   shouldComponentUpdate(nextProps: Readonly<any>): boolean {
@@ -47,73 +70,62 @@ export default class ReactAntSelectCascader extends Component<any> {
     return true;
   }
 
-  componentDidMount() {
+  private doQuery = (inIndex: number, inData: any): Promise<any> => {
     const { query } = this.props;
-    const { options, loading } = this.state;
-    loading[0] = true;
-    this.setState({ loading });
-    query[0](null)
+    const { itemsList, loadingList } = this.state;
+    const api = query[inIndex];
+    if (!api) return Promise.resolve();
+    loadingList[inIndex] = true;
+    this.setState({ loadingList });
+
+    return api(inData)
       .then(({ items }) => {
-        options[0] = items;
-        this.setState({ options });
+        itemsList[inIndex] = items;
+        this.setState({ itemsList });
       })
       .finally(() => {
-        loading[0] = false;
-        this.setState({ loading });
+        loadingList[inIndex] = false;
+        this.setState({ loadingList });
       });
+  };
+
+  componentDidMount() {
+    this.doQuery(0, null);
   }
 
   handleTemplate = ({ item, index }) => {
-    const { value, loading } = this.state;
-    return (
-      <ReactAntSelect
-        key={index}
-        items={item}
-        loading={loading[index]}
-        value={value[index]}
-        onChange={this.handleItemChange.bind(this, index)}
-        style={{ width: 200 }}
-      />
-    );
+    const { template } = this.props;
+    const { value, loadingList } = this.state;
+    const props = {
+      items: item,
+      loading: loadingList[index],
+      value: value![index],
+      onChange: this.handleItemChange.bind(this, index)
+    };
+    return template!({ item, index, props });
   };
 
   handleItemChange = (inIndex, inEvent) => {
-    const { query, onChange } = this.props;
-    const { options, loading } = this.state;
+    const { onChange } = this.props;
     const { value } = inEvent.target;
-    const api = query[inIndex + 1];
-    const _value = this.state.value;
+    const _value = this.state.value!;
+    const target = { value: _value };
     _value.forEach((_, index) => index > inIndex && _value.splice(index, 1));
     _value[inIndex] = value;
-
-    api && (loading[inIndex] = true);
-    const target = { value: _value };
-
-    this.setState({ ...target, loading });
-    onChange({ target });
-
-    if (api) {
-      api(value)
-        .then(({ items }) => {
-          options[inIndex + 1] = items;
-          this.setState({ options });
-        })
-        .finally(() => {
-          loading[inIndex] = false;
-          this.setState({ loading });
-        });
-    }
+    this.setState({ ...target });
+    onChange!({ target });
+    this.doQuery(inIndex + 1, value);
   };
 
   render() {
-    const { className, value, onChange, ...props } = this.props;
-    const { options } = this.state;
+    const { className, value, onChange, template, ...props } = this.props;
+    const { itemsList } = this.state;
 
     return (
       <ReactList
         nodeName="div"
         data-component={CLASS_NAME}
-        items={options}
+        items={itemsList}
         template={this.handleTemplate}
         className={classNames(CLASS_NAME, className)}
         {...props}
